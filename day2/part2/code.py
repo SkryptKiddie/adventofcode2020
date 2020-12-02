6395e605850bf89496e2e6365860261b37ec00db
Joshua <16878148+SkryptKiddie@users.noreply.github.com>
2020-12-02 14:59:10 +0800

    the actual part 2 code

--- day2/part2/code.py
+++ day2/part2/code.py
@@ -1,5 +1,5 @@
-# code for advent of code day 2 part 1
-# https://adventofcode.com/2020/day/2
+# code for advent of code day 2 part 2
+# https://adventofcode.com/2020/day/2#part2
 import sys, time
 start_time = time.time() # get the start time
 
@@ -12,14 +12,15 @@ def solve(file):
         pw_min = int(divider.partition("-")[0])
         pw_letter = str(divider.partition("-")[2].partition(' ')[2])
         pw_string = str(line.partition(":")[2])
-        if (pw_string.count(pw_letter) <= pw_max) == True: # does the string contain the maximum amount of pw_letter?
-            if (pw_string.count(pw_letter) >= pw_min) == True: # does the string contain the minimum amount of pw_letter?
-                counter += 1
-                print("{} VALID | {} {} {} {}".format(counter, pw_string, pw_letter, pw_max, pw_min)) # print the valid password and its specs
-            else: # doesn't match the minimum pw_letter amount
-                continue
-        else: # doesn't match the maximum pw_letter amount
-            continue
+        if pw_string[pw_min] == pw_letter: # check if pw_min is the valid first letter, otherwise check pw_max at else
+            if pw_string[pw_max] != pw_letter: # make sure pw_max isn't a match
+                counter+=1
+                print("{} VALID | {} {} {} {}".format(counter, pw_string, pw_letter, pw_max, pw_min))
+        else:
+            if pw_string[pw_max] == pw_letter: # check if pw_max is the valid first letter
+                if pw_string[pw_min] != pw_letter: # make sure pw_min isn't a match
+                    counter+=1
+                    print("{} VALID | {} {} {} {}".format(counter, pw_string, pw_letter, pw_max, pw_min))
 
 try:
     solve(file="pw_list.txt") # run the solve function
